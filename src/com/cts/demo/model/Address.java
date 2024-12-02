9714a43be0a9e119ea448cb61bbe462e9308308c
merajiiht <merajiiht@gmail.com>
2024-12-02 11:45:23 +0530

    Second commit

---
+++ src/com/cts/demo/model/Address.java
@@ -0,0 +1,20 @@
+package com.cts.demo.model;
+
+public class Address {
+	private String flatno;
+	private String colony;
+	public String getFlatno() {
+		return flatno;
+	}
+	public void setFlatno(String flatno) {
+		this.flatno = flatno;
+	}
+	public String getColony() {
+		return colony;
+	}
+	public void setColony(String colony) {
+		this.colony = colony;
+	}
+	
+
+}
